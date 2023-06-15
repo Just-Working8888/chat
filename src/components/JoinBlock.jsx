6e4185b0cde3efb64493f991a638c47fd40f9cbd
Just-Working8888 <justworking8888@gmail.com>
2023-06-15 19:46:59 +0600

    first commit

---
+++ src/components/JoinBlock.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import axios from 'axios';
+
+function JoinBlock({ onLogin }) {
+  const [roomId, setRoomId] = React.useState('');
+  const [userName, setUserName] = React.useState('');
+  const [isLoading, setLoading] = React.useState(false);
+
+  const onEnter = async () => {
+    if (!roomId || !userName) {
+      return alert('Неверные данные');
+    }
+    const obj = {
+      roomId,
+      userName,
+    };
+    setLoading(true);
+    await axios.post('/rooms', obj);
+    onLogin(obj);
+  };
+
+  return (
+    <div className="join-block">
+      <input
+        type="text"
+        placeholder="Room ID"
+        value={roomId}
+        onChange={(e) => setRoomId(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Ваше имя"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <button disabled={isLoading} onClick={onEnter} className="btn btn-success">
+        {isLoading ? 'ВХОД...' : 'ВОЙТИ'}
+      </button>
+    </div>
+  );
+}
+
+export default JoinBlock;
